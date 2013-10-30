b945da2116a8fd7f2140330dacb693fe1665d020
Sergio <sergiogoro86 at gmail dot com>
2013-10-30 22:51:12 +0100

    Correciones introducidas

--- 01-Objetos_en_Perl/POO_tradicional/submit/Alumnos.pl
+++ 01-Objetos_en_Perl/POO_tradicional/submit/Alumnos.pl
@@ -7,10 +7,45 @@ use PA::Alumnos;
 
 my $centro = shift || die "Falta el 1º argumento (centro)";
 my $cadenaAlumnos = shift || die "Falta el 2º arg (lista alumnos separados por coma ',')";
-#my @alumnos = @_ || die "Falta el 2º arg (lista alumnos)"; #NO, die diciendo que falta 2º argumento...
-    # ¿Cómo podría recoger el resto de elementos de @_ y guardarlos en un array?
 my @alumnos = split ",", $cadenaAlumnos;
+#my (@alumnos) = (@_) || die "Faltan los alumnos";
+#my @alumnos = @_ || die "Falta el 2º arg (lista alumnos)"; #NO, die diciendo que falta 2º argumento...
+    # ¿Cómo podría recoger el resto de elementos de @_ y guardarlos en un array? @arr = @_ no funciona, dies...
 
-my $centroEducativo = new PA::Alumnos($centro, \@alumnos);
+my $centroEducativo = new PA::Alumnos();                                # New objeto
+$centroEducativo->setCentro("UGR");                                     # Set centro
+$centroEducativo->setAlumnos(\@alumnos);                                # Set alumnos
 
-say Dump $centroEducativo;
+say "Centro: " . $centroEducativo->getCentro ;                          # Obtener centro
+say "Alumnos: " . join ", ", @{ $centroEducativo->{"_alumnos"} };       # Obtener alumnos
+#my $alumnos = join (", ", @{ $centroEducativo->{"_alumnos"} } );       # Obtener alumnos indirectamente
+#say "Alumnos: $alumnos";
+
+say "";
+
+say "Esquema del objeto \$centroEducativo:";
+say Dump $centroEducativo;                                              # Dump objeto
+
+
+
+
+
+# # Tests
+#   say "-"x5;
+#   say "say \$centroEducativo->getCentro";
+#   say $centroEducativo->getCentro;
+#   
+#   say "-"x5;
+#   say "say \$centroEducativo->{\"_centro\"}";
+#   say $centroEducativo->{"_centro"};
+#   
+#   say "-"x5;
+#   say "say foreach \@{ \$centroEducativo->{\"_alumnos\"} }";
+#   say foreach @{ $centroEducativo->{"_alumnos"} };
+#   
+#   say "-"x5;
+#   say "\$alumnosDevueltos_aref = \$centroEducativo->getAlumnos\n\t&\n\t\@alumnosDevueltos = \@{ \$alumnosDevueltos_aref }\n\t&\n\tsay \"<\$_>\" foreach \@alumnosDevueltos";
+#   my $alumnosDevueltos_aref = $centroEducativo->getAlumnos;
+#   my @alumnosDevueltos = @{ $alumnosDevueltos_aref };
+#   say "<$_>" foreach @alumnosDevueltos;
+
--- 01-Objetos_en_Perl/POO_tradicional/submit/lib/PA/Alumnos.pm
+++ 01-Objetos_en_Perl/POO_tradicional/submit/lib/PA/Alumnos.pm
@@ -3,19 +3,46 @@ package PA::Alumnos;
 use strict;
 use warnings;
 use feature 'say';
+use YAML qw(Dump);
 
 sub new {
     my $this = shift;
-    my $centro = shift;
-    my $alumnos = shift;
-    my $class = ref($this) || $this;
-    my $self = {
-        CENTRO => $centro,
-        ALUMNOS => $alumnos,
-    };
+    my $class = ref($this) || $this;    #Si $this es una referencia (porque se ha llamado a new desde otro Package (PA::Algo p.ej) ), 
+                                            # entonces guardamos esa referencia como la clase. (Para luego bless $self, $class)
+                                        # Si no es ninguna referencia, es que no se ha llamado desde otro Package, y la clase
+                                            # será ésta nativa/madre/yoquese (PA::Alumnos)
 
-    bless $self, $class;
-    return ($self);
+    my $self = {};
+    bless $self, $class;        # Class depende desde que punto de la jerarquía de clases se ha llamado
+                                    #$class = PA::Alumnos      OR      $class = PA::Algo (si llamamos a new desde PA::Algo)
+    #bless $self                # Obtendría la clase (le asignará el nombre) desde la cual llamamos a new.
+                                    # Si llamamos a ' centro = new PA::Alumnos' desde un Package PA::Algo, la clase
+                                        # por defecto será PA::Algo, en lugar de PA::Alumnos
+    
+    return $self;
 };
+
+sub setCentro {
+    my $self = shift;
+    $self->{"_centro"} = shift;
+}
+
+sub setAlumnos {
+    my $self = shift;
+    my $arr_ref = shift;
+    my @arr = @{ $arr_ref };
+    @{ $self->{"_alumnos"} } = @arr;
+}
+
+sub getCentro {
+    my $self = shift;
+    return $self->{"_centro"};
+}
+
+sub getAlumnos {
+    my $self = shift;
+    return \@{ $self->{"_alumnos"} };
+}
+
 
 1;
